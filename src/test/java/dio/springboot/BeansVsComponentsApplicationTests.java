bbb593b58d1da7f56c2f13687d00aa7f202e1010
joaojorge <joaojorgedosanjos@gmail.com>
2023-07-18 14:11:56 -0300

    Aula Beans e Components

--- src/test/java/dio/springboot/BeansVsComponentsApplicationTests.java
+++ src/test/java/dio/springboot/BeansVsComponentsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BeansVsComponentsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
